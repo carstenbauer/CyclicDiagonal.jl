d659ba7ffc6febae093a66fc6c920aa218818ba6
noponen <60381632+noponen@users.noreply.github.com>
2020-02-24 18:15:38 +0200

    Files generated by PkgTemplates

---
+++ src/CyclicDiagonal.jl
@@ -0,0 +1,5 @@
+module CyclicDiagonal
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CyclicDiagonal
+using Test
+
+@testset "CyclicDiagonal.jl" begin
+    # Write your own tests here.
+end
